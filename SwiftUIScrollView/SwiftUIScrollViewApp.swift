15e5952a628b2c60825f8ffdfab49f03e6adbcb3
Sinan Ulusan <sinanulusan99@gmail.com>
2021-03-03 00:26:57 +0300

    Initial Commit

---
+++ SwiftUIScrollView/SwiftUIScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollViewApp.swift
+//  SwiftUIScrollView
+//
+//  Created by Sinan Ulusan on 3.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
